cd0d904d732ee26490b3cc5a236de2026c3586d9
mika <kaszuba.mika@gmail.com>
2016-07-18 23:18:02 +0200

    missing rate of change code

---
+++ FinantialMathematics/RateOfChange.fs
@@ -0,0 +1,32 @@
+﻿module RateOfChange
+
+open FinantilaMathematics.Currency
+open LatestExchangeRates
+
+let rateOfChange sourceCurrency finalCurrency (rateTable:Currency->ExchanegRate) =
+    let (_, _, sourceRate) = sourceCurrency |> rateTable
+    let (_, _, finalRate) = finalCurrency |> rateTable
+    sourceRate / finalRate
+
+let exchangeCurrency (money:Money) (finalCurrency:Currency)
+    (rateTable:Currency->ExchanegRate):Money =
+    let (d, sourceCurrency ) = money
+    let rateOfChange' =
+        rateTable |> ( (sourceCurrency, finalCurrency) ||> rateOfChange )
+    (d * rateOfChange'), finalCurrency
+
+let exchange (amount:Money) (rate:ExchanegRate):Money =
+    let rsc,dc,r = rate
+    let a,sc = amount
+    if sc<>rsc then
+        "Currency mismatch error! Currency rate is not for the source currency"
+        |> CurrencyException |> raise
+    a * r, dc
+
+
+let add (amount1:Money) (amount2:Money) =
+    let d1,c1 = amount1
+    let d2,c2 = amount2
+    let sum = if c1=c2 then d1 + d2 else d1 + d2 * (latestRate (c1, c2))
+    sum, c1
+
---
+++ FinantialMethematics.Test/RateOfChangeTest.fs
@@ -0,0 +1,51 @@
+﻿module RateOfChangeTest
+open Xunit
+open FinantilaMathematics.Currency
+open FsCheckProperties
+open Swensen.Unquote
+open RateOfChange
+
+let testRateTable = function
+    | Eur -> Eur,Usd,1.23M
+    | Pln -> Pln,Usd,0.67M
+    | Ypy -> Ypy,Usd,0.12M
+    | Gbp -> Gbp,Usd,1.4M
+    | Usd -> Usd,Usd,1M
+
+[<Theory>]
+[<InlineData(1, 1.83582)>]
+let ``convert eur to pln`` (eurAmount:decimal) (expected:decimal) =
+    let c = Currency.Eur
+    let m:Money = (eurAmount, c)
+    let actual, finalCurrency = exchangeCurrency m Pln testRateTable
+    Assert.Equal (Currency.Pln, finalCurrency)
+    Assert.InRange ( actual, expected - 0.0001M , expected + 0.0001M)
+
+[<RandomTypeProperty>]
+let ``converting to the same currency gives the same amount`` (m:Money) =
+    let (amount,cur) = m
+    let actual, finalCurrency = exchangeCurrency m cur testRateTable
+    test <@ finalCurrency = cur @>
+    test <@ actual = amount @>
+
+[<Fact>]
+let ``currency mismatch throws Currency exception`` () =
+    let m = 10M, Pln
+    let er = Usd,Ypy,1.2M
+    let ex =
+        Assert.Throws<CurrencyException> (fun () -> exchange m er |> ignore )
+    ex.Message.Contains("Currency mismatch error! Currency rate is not for the
+        source currency");
+
+[<Fact>]
+let ``exchange yens to punds`` () =
+    let y:Money = 150M,Ypy
+    let r:ExchanegRate = Ypy,Gbp,0.0065M
+    let d,c = y |> exchange <|r
+    d =! 0.975M
+    c =! Gbp
+
+[<RandomTypeProperty>]
+let ``currency of sum `` (a1:Money) (a2:Money) =
+    let d, c = add a1 a2
+    c =! (a1 |> snd)
